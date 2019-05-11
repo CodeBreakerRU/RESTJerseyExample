8f2193f0f3811f749e7af27656d37db70bf6e799
cs-riley <liyanaarachchirasika@gmail.com>
2019-05-11 23:44:23 +0530

    finale update

---
+++ src/com/test/CtoFService.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/RESTService")
+public class CtoFService {
+	
+	@GET
+	@Path("/convertCtoF")
+	@Produces("application/xml")
+	
+	public String convertCtoF() {
+		
+		Double fahrenheit;
+		Double celsius = 36.8;
+		fahrenheit = ((celsius * 9)/5) + 32;
+		
+		String result = "@Produces(\"application/xml\") Output: \n\nC to F converter Output : \n\n" + fahrenheit;
+		return "<ctofservice>" + "<celsius>" + celsius + "</celsius>" + "<ctofoutput>" + result + "</ctofoutput>" + "</ctofservice>";
+	}
+ 
+}
